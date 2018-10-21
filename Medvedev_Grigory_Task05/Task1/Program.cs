a2c9c0f880f883c2660ef8b3b35be393b0a772ca
MedvedevGrigory <44243958+MedvedevGrigory@users.noreply.github.com>
2018-10-21 23:47:45 +0300

    Task05/Task1

---
+++ Medvedev_Grigory_Task05/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter surname:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter patronymic:");
+            string patronymic = Console.ReadLine();
+
+            Console.WriteLine("Enter birthdate in format \"dd.mm.yyyy\"");
+            if (!DateTime.TryParse(Console.ReadLine(), out DateTime birthdate))
+            {
+                Console.WriteLine("Invalid expression.");
+            }
+
+            try
+            {
+                User user = new User(surname, name, patronymic, birthdate);
+
+                Console.WriteLine($"\nSurname - {user.Surname}\n" +
+                    $"Name - {user.Name}\n" +
+                    $"Patronymic - {user.Patronymic}\n" +
+                    $"Birthdate - {user.Birthdate.ToShortDateString()}\n" +
+                    $"Age - {user.Age}");
+            }
+            catch
+            {
+                Console.WriteLine("Error. Invalid data entered.");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Medvedev_Grigory_Task05/Task1/User.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    class User
+    {
+
+
+        private string surname;
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                string pattern = "\\W|\\d";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    throw new Exception("Invalid expression.");
+                }
+                surname = value;
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                string pattern = "\\W\\d";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    throw new Exception("Invalid expression.");
+                }
+                name = value;
+            }
+        }
+
+        private string patronymic;
+
+        public string Patronymic
+        {
+            get
+            {
+                return patronymic;
+            }
+            set
+            {
+                string pattern = "\\W\\d";
+                if (Regex.IsMatch(value, pattern))
+                {
+                    throw new Exception("Invalid expression.");
+                }
+                patronymic = value;
+            }
+        }
+
+        private DateTime birthdate;
+
+        public DateTime Birthdate
+        {
+            get
+            {
+                return birthdate;
+            }
+            set
+            {
+                if (birthdate > DateTime.Now)
+                {
+                    throw new Exception("Error. Birthdate cannot be greater than tha current date.");
+                }
+                birthdate = value;
+            }
+        }
+
+        public User(string surname, string name, string patronymic, DateTime birthdate)
+        {
+            Surname = surname;
+            Name = name;
+            Patronymic = patronymic;
+            Birthdate = birthdate;
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                age = DateTime.Now.Year - birthdate.Year;
+                if (DateTime.Now.Month < birthdate.Month)
+                {
+                    age--;
+                }
+                if (DateTime.Now.Month == birthdate.Month)
+                {
+                    if (DateTime.Now.Day < birthdate.Day)
+                    {
+                        age--;
+                    }
+                }
+                return age;
+            }
+        }
+    }
+}
