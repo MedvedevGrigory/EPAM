057c380dba003b90f15a40788739cb353a177111
MedvedevGrigory <44243958+MedvedevGrigory@users.noreply.github.com>
2018-10-26 02:31:22 +0300

    Add Task02/Task4 and Task02/Task5

---
+++ Medvedev_Grigory_Task02/Task4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N:");
+            int N;
+            while (!int.TryParse(Console.ReadLine(), out N) | !CheckPositive(N))
+            {
+                Console.WriteLine("Error. Invalid value. Expected positive number.");
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                Output(i, N);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Output(int M, int N)
+        {
+            //Console.Clear();
+
+            for (int i = 1; i <= M; i++)
+            {
+                for (int k = M; k < N; k++)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = 1; j <= M - i; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = 1; j < i * 2; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool CheckPositive(int value)
+        {
+            return value > 0;
+        }
+    }
+}
