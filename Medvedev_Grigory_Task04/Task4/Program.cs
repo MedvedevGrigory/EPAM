0f988819bb4c37828bb40341c070334eeb850e79
MedvedevGrigory <44243958+MedvedevGrigory@users.noreply.github.com>
2018-10-26 07:18:14 +0300

    Add Task04/Task4, Task04/Task5 and Task04/Task6

---
+++ Medvedev_Grigory_Task04/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+            int N = 10000;
+
+            Stopwatch timer1 = Stopwatch.StartNew();
+            for (int i = 0; i < N; i++)
+            {
+                str += "*";
+            }
+            long time1 = timer1.ElapsedMilliseconds;
+
+            Stopwatch timer2 = Stopwatch.StartNew();
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append("*");
+            }
+            long time2 = timer2.ElapsedMilliseconds;
+
+
+            Console.WriteLine($"Time of work \"String\" = {time1}");
+            Console.WriteLine($"Time of work \"StringBuilder\" = {time2}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Medvedev_Grigory_Task04/Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text with HTML-tegs:");
+            string text = Console.ReadLine();
+
+            string pattern = @"<[\w\W]*>";
+            string changeText = Regex.Replace(text, pattern, "_");
+
+            Console.WriteLine(changeText);
+
+            Console.ReadKey();
+        }
+    }
+}
