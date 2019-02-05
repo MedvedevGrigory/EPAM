03e9f798116d0ac0798d311f7fcf3fc1d88cd8e0
MedvedevGrigory <44243958+MedvedevGrigory@users.noreply.github.com>
2019-02-05 11:49:41 +0300

    Add snake
    Task1

---
+++ Medvedev_Grigory_Snake/Medvedev_Grigory_Snake/FoodCreator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medvedev_Grigory_Snake
+{
+    class FoodCreator
+    {
+        public FoodCreator(int MapWidth, int MapHeight, char sym)
+        {
+            this.MapWidth = MapWidth;
+            this.MapHeight = MapHeight;
+            this.sym = sym;
+        }
+
+        int MapWidth;
+        int MapHeight;
+        char sym;
+
+        Random random = new Random();
+
+        internal Point CreateFood()
+        {
+            int x = random.Next(2, MapWidth - 2);
+            int y = random.Next(2, MapHeight - 2);
+
+            return new Point(x, y, sym);
+        }
+    }
+}
---
+++ Medvedev_Grigory_Snake/Medvedev_Grigory_Snake/Walls.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medvedev_Grigory_Snake
+{
+    class Walls
+    {
+        public Walls(int MapWidth, int MapHeight)
+        {
+            wallList = new List<Figure>();
+
+            HorizontalLine upLine = new HorizontalLine(0, MapWidth - 2, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, MapWidth - 2, MapHeight - 1, '+');
+            VerticalLine leftLine = new VerticalLine(0, MapHeight - 1, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, MapHeight - 1, MapWidth - 2, '+');
+
+            wallList.Add(upLine);
+            wallList.Add(downLine);
+            wallList.Add(leftLine);
+            wallList.Add(rightLine);
+        }
+
+        List<Figure> wallList;
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach (Figure wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
